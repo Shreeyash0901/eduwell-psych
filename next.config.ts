801fb89de58c3541564efe7344120acef30d5aa3
Shreeyash Helchal <itsmeyash02@gmail.com>
2026-08-16 10:10:40 +0530

    chore: configure GitHub Pages deployment123

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/eduwell-psych",
+  basePath: "/eduwell-psych/dashboard",
   images: {
     unoptimized: true,
   },
